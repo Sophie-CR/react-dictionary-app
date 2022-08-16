4370fdb2d43b9acb7aee76d94340c27be4835995
Sophie-CR <104981706+Sophie-CR@users.noreply.github.com>
2022-08-16 11:04:51 +0100

    Numbered the definitions, styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,23 @@
 import React from "react";
-import "./Results.css";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.data);
   return (
     <div className="meaning">
-      <h3>{props.data.partOfSpeech}</h3>
+      <h3>
+        <em>{props.data.partOfSpeech}</em>
+      </h3>
       {props.data.definitions.map(function (definition, index) {
         return (
-          <div key={index}>
-            <div>{definition.definition}</div>
-            <div>{definition.example}</div>
+          <div key={index} className="definition">
+            <div>
+              <span>{index + 1}: </span>
+              {definition.definition}
+            </div>
+            <div>
+              <em>{definition.example}</em>
+            </div>
           </div>
         );
       })}
